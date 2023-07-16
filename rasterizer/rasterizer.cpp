e9dd0ef0e6550ce5870d23c07005c54951768f9b
AliMctavish <alifadil4141@gmail.com>
2023-07-17 00:04:40 +0300

    adding the brensham algorithim

--- rasterizer/rasterizer.cpp
+++ rasterizer/rasterizer.cpp
@@ -2,13 +2,17 @@
 //
 #include "tgaimage.h"
 #include <iostream>
+#include<windows.h>    
 
 
+//RANDOM COLORS FOR LINE DRAWING
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor yellow = TGAColor(0, 255, 0, 255);
 const TGAColor green = TGAColor(0, 255, 255, 255);
 
-void line(float x0, float y0, float x1, float y1, TGAImage& image, TGAColor color)
+
+//DDA ALGORITHIM IMPLEMENTAION
+void DrawByDDA(float x0, float y0, float x1, float y1, TGAImage& image, TGAColor color)
 {
 	float invX = x1 - x0;
 	float invY = y1 - y0;
@@ -53,12 +57,26 @@ void line(float x0, float y0, float x1, float y1, TGAImage& image, TGAColor colo
 	}
 }
 
+//BRENSHAM ALGORITHIM IMPLEMENTAION
+void DrawByBresnham(int x0, int y0 , int x1 , int y1 , TGAImage& image, TGAColor color)
+{
+	for (int x = x0; x <= x1; x += 1)
+	{
+		float t = (x - x0) / (float)(x1 - x0);
+		int y = y0 * (1. - t) + y1 * t;
+		image.set(x, y, color);
+	}
+}
+
 int main()
 {
 	TGAImage image(200, 200, TGAImage::RGB);
-	line(13, 20, 100, 40, image, red);
-	line(100, 40, 13, 80, image, red);
-	line(13, 80, 13, 20, image, red);
-	image.write_tga_file("tringle27323.tga");
+	DrawByBresnham(20, 30, 50, 80, image, red);
+	DrawByBresnham(20, 30, 50, 20, image, red);
+	DrawByBresnham(50, 20, 50, 80, image, green);
+	image.write_tga_file("withbrensham.tga");
+	system("cd E:");
+	system("cd E:/Program Files/Aseprite>Aseprite.exe");
+
 	return 0;
 }
