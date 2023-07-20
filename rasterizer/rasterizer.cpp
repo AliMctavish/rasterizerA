ef49421b0411c3e36ce0dd086518f01967aa9a1d
AliMctavish <alifadil4141@gmail.com>
2023-07-20 11:04:34 +0300

    worked !

--- rasterizer/rasterizer.cpp
+++ rasterizer/rasterizer.cpp
@@ -2,21 +2,20 @@
 //
 #include "tgaimage.h"
 #include <iostream>
-#include <windows.h>    
 
 
 //RANDOM COLORS FOR LINE DRAWING
 const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor yellow = TGAColor(0, 255, 0, 255);
-const TGAColor green = TGAColor(0, 255, 255, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+const TGAColor cyan = TGAColor(0, 255, 255, 255);
 
 
 //DDA ALGORITHIM IMPLEMENTAION
-void DrawByDDA(float x0, float y0, float x1, float y1, TGAImage& image, TGAColor color)
+void DrawByDDA(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 {
-	for (float t = 0.; t < 1.; t += .01) {
-		int x = x0 * t;
-		int y = y0 * t;
+	for (float t = 0.; t < 1.; t += .001) {
+		int x = x0 + (x1 - x0) * t;
+		int y = y0 + (y1 - y0) * t;
 		image.set(x, y, color);
 	}
 }
@@ -24,23 +23,37 @@ void DrawByDDA(float x0, float y0, float x1, float y1, TGAImage& image, TGAColor
 //BRENSHAM ALGORITHIM IMPLEMENTAION
 void DrawByBresnham(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 {
-	for (int x = x0; x <= x1; x += 1)
+	int Xdeff = x1 - x0;
+	int Ydeff = y1 - y0;
+	if (std::abs(Xdeff) > std::abs(Ydeff))
 	{
-		float t = (x - x0) / (float)(x1 - x0);
-		int y = y0 * (1. - t) + y1 * t;
-		image.set(x, y, color);
+		for (int x = x0; x <= x1; x += 1)
+		{
+			float t = (x - x0) / (float)(x1 - x0);
+			int y = y0 * (1. - t) + y1 * t;
+			image.set(x, y, color);
+		}
+	}
+	else
+	{
+		for (int y = y0; y <= y1; y += 1)
+		{
+			float t = (y - y0) / (float)(y1 - y0);
+			int x = x0 * (1. - t) + x1 * t;
+			image.set(x, y, color);
+		}
 	}
 }
 
 
 void DrawBySlope(int point, int slopex, int slopey, TGAImage& image, TGAColor color)
 {
-	again:
+again:
 	for (int i = 0; i <= slopex; i++)
 	{
 		point++;
 		for (int j = 0; j <= slopey; j++)
-		{ 
+		{
 			point++;
 			if (point > 100)
 			{
@@ -50,7 +63,7 @@ void DrawBySlope(int point, int slopex, int slopey, TGAImage& image, TGAColor co
 			{
 				point++;
 				image.set(point, point, color);
-			
+
 				goto again;
 			}
 		}
@@ -60,11 +73,13 @@ void DrawBySlope(int point, int slopex, int slopey, TGAImage& image, TGAColor co
 
 int main()
 {
+	int x = 0;
 	TGAImage image(200, 200, TGAImage::RGB);
-	DrawBySlope(30,2,.1, image, red);
-	DrawByDDA(30,60,70,80, image, red);
-	DrawByDDA(70,80,100,120, image, red);
-	DrawByDDA(100,120,30,60, image, red);
+	for (int i = 1; i < 100; i++)
+	{
+		DrawByBresnham(170 / i, 10, 30 * x, 180, image, green);
+		x++;
+	}
 	image.write_tga_file("withbrensham.tga");
 	return 0;
 }
